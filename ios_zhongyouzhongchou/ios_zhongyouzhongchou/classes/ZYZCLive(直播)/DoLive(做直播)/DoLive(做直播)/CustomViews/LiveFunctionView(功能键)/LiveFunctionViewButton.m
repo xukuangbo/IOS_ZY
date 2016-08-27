f17482f773c6eef6f1fe44236f9bb19e05859639
syk <270081841@qq.com>
2016-08-27 15:17:07 +0800

    功能键优化

---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCLive(直播)/DoLive(做直播)/DoLive(做直播)/CustomViews/LiveFunctionView(功能键)/LiveFunctionViewButton.m
@@ -0,0 +1,21 @@
+//
+//  LiveFunctionViewButton.m
+//  ios_zhongyouzhongchou
+//
+//  Created by syk on 16/8/27.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "LiveFunctionViewButton.h"
+
+@implementation LiveFunctionViewButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
