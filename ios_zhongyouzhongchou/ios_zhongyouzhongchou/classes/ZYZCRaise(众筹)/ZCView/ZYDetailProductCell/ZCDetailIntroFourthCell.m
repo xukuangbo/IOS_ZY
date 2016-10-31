fc1b28f7692ab9357cf8862fcbf6feb8fa5c26d7
liuliang <liuliang@liuliang.local>
2016-10-31 20:46:22 +0800

    众筹详情小细节修改

---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCRaise(众筹)/ZCView/ZYDetailProductCell/ZCDetailIntroFourthCell.m
@@ -0,0 +1,21 @@
+//
+//  ZCDetailIntroFourthCell.m
+//  ios_zhongyouzhongchou
+//
+//  Created by liuliang on 16/10/31.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "ZCDetailIntroFourthCell.h"
+
+@implementation ZCDetailIntroFourthCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
