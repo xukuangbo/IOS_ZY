d776499abbdab302608608880ec79ee7d3098e47
liuliang <liuliang@liuliang.local>
2016-11-01 11:08:34 +0800

    MB的界面bug

--- ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCRaise(众筹)/ZCView/ZYDetailProductCell/ZCDetailIntroFourthCell.m
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCRaise(众筹)/ZCView/ZYDetailProductCell/ZCDetailIntroFourthCell.m
@@ -12,7 +12,7 @@
 @property (nonatomic, strong) UILabel *moneyLab;       //金额
 @property (nonatomic, strong) UILabel  *rateLab;       //支持比例
 @property (nonatomic, strong) UILabel  *limitLab;      //限额标签
-@property (nonatomic, strong) UIView   *supportLab; //支持的人
+@property (nonatomic, strong) UILabel  *supportLab; //支持的人
 @property (nonatomic, strong) UIView   *separateView;  //分割线
 @property (nonatomic, strong) UIView   *supportUsersView; //支持的人
 @property (nonatomic, strong) UIButton *supportBtn;       //支持按钮
@@ -38,6 +38,7 @@
     
     //金额
     _moneyLab=[ZYZCTool createLabWithFrame:CGRectMake(0, self.titleLab.top, self.bgImg.width, self.titleLab.height) andFont:[UIFont systemFontOfSize:15.f] andTitleColor:[UIColor blackColor]];
+    _moneyLab.textAlignment=NSTextAlignmentRight;
     [self.bgImg addSubview:_moneyLab];
     
     //金额比例
@@ -45,13 +46,17 @@
     [self.bgImg addSubview:_rateLab];
     
     //限额人数
-    _limitLab=[ZYZCTool createLabWithFrame:CGRectMake(KEDGE_DISTANCE, _rateLab.bottom+KEDGE_DISTANCE, 0, 20) andFont:[UIFont systemFontOfSize:13.f] andTitleColor:[UIColor ZYZC_TextGrayColor]];
+    _limitLab=[ZYZCTool createLabWithFrame:CGRectMake(KEDGE_DISTANCE, _rateLab.bottom+KEDGE_DISTANCE, 0, 20) andFont:[UIFont systemFontOfSize:15.f] andTitleColor:[UIColor ZYZC_TextGrayColor]];
     [self.bgImg addSubview:_limitLab];
     
     //参与人数
     _supportLab = [ZYZCTool createLabWithFrame:CGRectMake(0, _rateLab.bottom+KEDGE_DISTANCE, 0, 20) andFont:_limitLab.font andTitleColor:_limitLab.textColor];
     [self.bgImg addSubview:_supportLab];
     
+    _separateView = [UIView lineViewWithFrame:CGRectMake(0, 2.5, 1.0, 15) andColor:nil];
+    [self.bgImg addSubview:_separateView];
+    _separateView.hidden=YES;
+
     _supportUsersView=[[UIView alloc]initWithFrame:CGRectMake(KEDGE_DISTANCE, _limitLab.bottom+KEDGE_DISTANCE, 0, 0)];
     [self.bgImg addSubview:_supportUsersView];
     
@@ -65,12 +70,40 @@
 
 - (void) setTogtherModel:(ReportModel *)togtherModel
 {
-    _togtherModel=togtherModel;
+    _togtherModel = togtherModel;
+    _moneyLab.text = [NSString stringWithFormat:@"%@",togtherModel.price];
+    CGFloat money_rate=0.0;
+    if ([togtherModel.sumPrice floatValue]>0) {
+        money_rate = [togtherModel.price floatValue] / [togtherModel.sumPrice floatValue];
+    }
+    _rateLab.text = [NSString stringWithFormat:@"支持%.f旅费",money_rate];
+    _limitLab.text = [NSString stringWithFormat:@"限额:%@位",togtherModel.people];
+    _supportLab.text = [NSString stringWithFormat:@"已报名%ld位",togtherModel.users.count];
+    
+    
+   
+    
 }
 
 -(void) support:(UIButton *)button
 
 {
 }
+
+#pragma mark --- 改变文字样式
+-(NSAttributedString *)customStringByString:(NSString *)str
+{
+    NSMutableAttributedString *attrStr=[[NSMutableAttributedString alloc]initWithString:str];
+    NSRange range1=[str rangeOfString:@":"];
+    
+    NSRange range2=[str rangeOfString:@"位"];
+    
+    if (str.length) {
+        [attrStr addAttribute:NSFontAttributeName value:[UIFont boldSystemFontOfSize:15.f] range:NSMakeRange(range1.location+1, range2.location-range1.location-1)];
+        [attrStr addAttribute:NSForegroundColorAttributeName value:[UIColor blackColor] range:NSMakeRange(range1.location+1, range2.location-range1.location-1)];
+    }
+    return  attrStr;
+}
+
 
 @end
