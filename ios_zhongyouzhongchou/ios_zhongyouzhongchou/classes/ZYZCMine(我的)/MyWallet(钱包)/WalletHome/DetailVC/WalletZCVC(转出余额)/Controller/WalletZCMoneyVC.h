835dbbb1b5854400aa1b446904342f2342475966
syk <270081841@qq.com>
2016-11-07 17:44:10 +0800

    钱包余额初级搭建

---
+++ ios_zhongyouzhongchou/ios_zhongyouzhongchou/classes/ZYZCMine(我的)/MyWallet(钱包)/WalletHome/DetailVC/WalletZCVC(转出余额)/Controller/WalletZCMoneyVC.h
@@ -0,0 +1,13 @@
+//
+//  WalletZCMoneyVC.h
+//  ios_zhongyouzhongchou
+//
+//  Created by syk on 16/11/7.
+//  Copyright © 2016年 liuliang. All rights reserved.
+//
+
+#import "ZYZCBaseViewController.h"
+
+@interface WalletZCMoneyVC : ZYZCBaseViewController
+
+@end
